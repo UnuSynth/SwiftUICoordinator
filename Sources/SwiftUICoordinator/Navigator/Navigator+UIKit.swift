6ce0ae7798671f49d85ae7113499c59543067f2f
aabdyshev <aabdyshev@devcats.kg>
2024-05-15 17:34:52 +0600

    fixed files location

--- Sources/SwiftUICoordinator/Navigator/Navigator+UIKit.swift
+++ Sources/SwiftUICoordinator/Navigator/Navigator+UIKit.swift
@@ -1,6 +1,5 @@
 import Foundation
 import UIKit
-import SwiftUICoordinator
 
 public extension Navigator where Self: RouterUIViewFactory {
     
--- Sources/SwiftUICoordinator/Routing/RouterUIViewFactory.swift
+++ Sources/SwiftUICoordinator/Routing/RouterUIViewFactory.swift
@@ -1,6 +1,5 @@
 import Foundation
 import UIKit
-import SwiftUICoordinator
 
 @MainActor
 public protocol RouterUIViewFactory {
